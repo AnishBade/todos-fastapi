3dd1ba8dc10308534de95e93e4a425a4eaec99e8
Anish Bade <badeanish6045@gmail.com>
2023-12-26 06:21:53 +0545

    update functionality added

--- model.py
+++ model.py
@@ -6,4 +6,11 @@ class Todo(BaseModel):
     item: str
 
     class Config:
-        Schema_extra = {"Example": {"id": 1, "item": "Example schema!"}}
+        json_schema_extra = {"Example": {"id": 1, "item": "Example schema!"}}
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        json_schema_extra = {"example": {"item": "Read the next chapter of the book."}}
--- todo.py
+++ todo.py
@@ -1,25 +1,41 @@
 from fastapi import APIRouter, Path
 
-from model import Todo
+from model import Todo, TodoItem
+
 todo_router = APIRouter()
 
 todo_list = []
+
+
 @todo_router.post("/todo")
-async def add_todo(todo:Todo) -> dict:
+async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
+
     return {"message": "Todo added succesfully"}
 
+
 @todo_router.get("/todo")
-async def retrieve_todos()->dict:
+async def retrieve_todos() -> dict:
     return {"todos": todo_list}
 
+
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = Path(..., title="TheID of the todo to retrieve.")) -> dict:
+async def get_single_todo(
+    todo_id: int = Path(..., title="TheID of the todo to retrieve.")
+) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
-            return {
-            "todo": todo
-            }
-    return {
-    "message": "Todo with supplied ID doesn't exist."
-    }
+            return {"todo": todo}
+    return {"message": "Todo with supplied ID doesn't exist."}
+
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(
+    todo_data: TodoItem,
+    todo_id: int = Path(..., title="The ID of the todo to be updated"),
+) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {"message": "Todo updated succesfully."}
+    return {"message": "Todo with supplied ID doesn't exist."}
